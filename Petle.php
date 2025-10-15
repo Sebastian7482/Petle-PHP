9c82752ca9a9a7ee119e8de166fcfd529e2a2d48
Sebastian Ćwienczek <seba.cwienczek09@gmail.com>
2025-10-15 10:55:45 +0200

    Pierwszy komit

---
+++ Petle.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pętle PHP</title>
+</head>
+    <body>
+        <?php
+        $a = -2;
+        while ($a > -7) {
+            $a--;
+            echo "$a <br>";
+        }
+
+        for ($i=1; $i < 6; $i++) { 
+            echo "Paragraf $i <br>";
+        }
+
+        ?>
+    </body>
+</html>
